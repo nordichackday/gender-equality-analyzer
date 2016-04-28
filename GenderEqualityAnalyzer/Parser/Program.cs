29d5b825347dd1fe508ad7aee052c85026d6675a
Sebastian Gunnarsson <sebastian.gunnarsson@sverigesradio.se>
2016-04-28 17:08:37 +0200

    Hämtar nu publisheddate och title från xml.

--- GenderEqualityAnalyzer/Parser/Program.cs
+++ GenderEqualityAnalyzer/Parser/Program.cs
@@ -24,55 +24,57 @@ namespace Parser
             var xml = GetSRXML().Result;
 
             XNamespace _xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
-
+            XNamespace _xmlnsn = "http://www.google.com/schemas/sitemap-news/0.9";
             Console.WriteLine(xml.Descendants(XName.Get("url")));
+            
+            var articles = from x in xml.Elements(_xmlns + "urlset").Elements(_xmlns + "url")
+                select new Article()
+                {
+                    Url = x.Element(_xmlns + "loc").Value,
+                    CreatedDate = DateTime.Now,
+                    PublishedDate =
+                        GetDateTimeOrDefault(x.Element(_xmlnsn + "news").Element(_xmlnsn + "publication_date").Value),
+                    Title = x.Element(_xmlnsn + "news").Element(_xmlnsn + "title").Value
+                };
 
-            var urls = from x in xml.Elements(_xmlns + "urlset").Elements(_xmlns + "url")
-                select x.Element(_xmlns + "loc").Value;
 
-
-            var articles = Parse(urls);
-
-            Console.WriteLine();
+            articles = Parse(articles);
+            Console.WriteLine("");
 
         }
 
-        static IEnumerable<Article> Parse(IEnumerable<string> urls)
+        private static DateTime GetDateTimeOrDefault(string date)
+        {
+            DateTime retval;
+            return DateTime.TryParse(date, out retval) ? retval : DateTime.MinValue;
+        }
+        static IEnumerable<Article> Parse(IEnumerable<Article> articles)
         {
             var tasks = new List<Task<Article>>();
 
-            foreach (var url in urls)
+            foreach (var article in articles)
             {
-                var task = Parse(url);
+                var task = Parse(article);
                 tasks.Add(task);
             }
 
             Task.WaitAll(tasks.ToArray());
-
             return tasks.Select(x => x.Result);
         }
 
-        static async Task<Article> Parse(string url)
+        static async Task<Article> Parse(Article article)
         {
             var httpClient = new HttpClient();
 
-            var html = await httpClient.GetStringAsync(url);
+            var html = await httpClient.GetStringAsync(article.Url);
 
             var regex = new Regex(@"<meta property=\""og:image\"" content=\""([a-zA-Z0-9\/\:\.\-_]*)", RegexOptions.IgnoreCase);
 
             var match = regex.Match(html);
             if (match.Success)
-            {
-                var img = match.Groups[1].Value;
-                Console.WriteLine(img);
+                article.ImageUrl = match.Groups[1].Value;
 
-                return new Article()
-                {
-                    ImageUrl = img
-                };
-            }
-            return new Article();
-
+            return article;
         }
 
         private static async Task<XDocument> GetSRXML()
@@ -80,7 +82,6 @@ namespace Parser
             var httpClient = new HttpClient();
 
             return  XDocument.Parse(await httpClient.GetStringAsync("http://sverigesradio.se/sida/newssitemap.aspx"));
-
             
         }
     }
