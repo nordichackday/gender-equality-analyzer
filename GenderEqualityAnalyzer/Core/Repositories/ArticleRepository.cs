f6525f6f939e641940de58bd48584502595cf441
Militz <suneson@gmail.com>
2016-05-01 07:37:30 +0200

    Cleanup

--- GenderEqualityAnalyzer/Core/Repositories/ArticleRepository.cs
+++ GenderEqualityAnalyzer/Core/Repositories/ArticleRepository.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Core.DataContext;
 
